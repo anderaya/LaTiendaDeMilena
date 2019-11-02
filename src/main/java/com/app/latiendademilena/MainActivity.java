2172dac605b8a2d93e59a88a092c9e2bea78b974
anderlya <acevedo.anderson@gmail.com>
2019-11-02 13:49:44 -0500

    0

--- src/main/java/com/app/latiendademilena/MainActivity.java
+++ src/main/java/com/app/latiendademilena/MainActivity.java
@@ -1,36 +1,53 @@
 package com.app.latiendademilena;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Button iniciar;
     TextView registrar, recuperar;
+    private FirebaseAuth mAuth;
+    private EditText Contraseña,Correo;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        mAuth = FirebaseAuth.getInstance();
+        //boton iniciar
         iniciar = findViewById(R.id.button2);
         iniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent intent = new Intent(getApplicationContext(), menu.class);
-                startActivity(intent);
-
+                iniciarss();
             }
         });
-
+        //boton registrar
         registrar = (TextView) findViewById(R.id.textView7);
         registrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //boton recuperar cuenta
         recuperar = (TextView) findViewById(R.id.textView5);
         recuperar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,5 +71,96 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //casillas para guardar datos
+        Correo =(EditText)findViewById(R.id.editText3);
+        Contraseña = (EditText)findViewById(R.id.editText4);
+
     }
+
+
+    //objeto de tipo usuario activo
+    private usuarioActivo us=new usuarioActivo();
+    private DatabaseReference databaseReference;
+    String contraseña,correo;
+
+    // funcion iniciar sesión
+    private void iniciarss(){
+        contraseña= Contraseña.getText().toString().trim();
+        correo= Correo.getText().toString().trim();
+        us.setCorreo(correo);
+
+        if(TextUtils.isEmpty(correo)){
+            Toast.makeText(this,"Falta ingresar un correo",Toast.LENGTH_LONG).show();
+            return ;
+        }
+        if(TextUtils.isEmpty(contraseña)){
+            Toast.makeText(this,"Falta ingresar la contraseña",Toast.LENGTH_LONG).show();
+
+            return ;
+        }
+
+        mAuth.signInWithEmailAndPassword(correo,contraseña).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this,"Se ha iniciado sesión",Toast.LENGTH_LONG).show();
+                    finish();
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    // Revisar en la base de datos si existe el usuario
+                    databaseReference= FirebaseDatabase.getInstance().getReference();
+                    databaseReference.child("usuario").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.exists()){
+                                dataSnapshot.getChildren();
+                                for(DataSnapshot ds:dataSnapshot.getChildren()){
+                                    //mejorar para obtener multiples resultados
+                                    if(ds.child("correo").getValue().toString().equals(us.getCorreo())) {
+
+                                        // Almacenar las coordenadas
+                                        us.setLongitud(ds.child("longitud").getValue().toString());
+                                        us.setLatitud(ds.child("latitud").getValue().toString());
+                                        us.setTipouser(ds.child("tipouser").getValue().toString());
+
+
+                                        if(ds.child("tipouser").getValue().toString().equals("admn")){
+                                            finish();
+                                            Intent intent = new Intent(getApplicationContext(), menuadm.class);
+                                            startActivity(intent);
+                                        }else{
+                                            finish();
+                                            Intent intent = new Intent(getApplicationContext(), menu.class);
+                                            startActivity(intent);
+                                        }
+                                        return;
+
+                                    }
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+
+                }else{
+                    Toast.makeText(MainActivity.this,"Correo o contraseña incorrecta "+contraseña,Toast.LENGTH_LONG).show();
+
+                    // Log.d(TAG, "onComplete: Failed=" + task.getException().getMessage());
+
+                }
+            }
+        });
+
+    }
+
+
+
 }
--- src/main/java/com/app/latiendademilena/registrar.java
+++ src/main/java/com/app/latiendademilena/registrar.java
@@ -1,24 +1,58 @@
 package com.app.latiendademilena;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class registrar extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class registrar extends AppCompatActivity implements  LocationListener{
 
     ImageView regresar;
-    Button registrar;
+    Button registrar,ingresarubicaciones;
+    private FirebaseAuth mAuth;
+    private EditText Usuario, Contraseña,Correo,Direccion;
+
+    LocationManager locationManager;
+
+    public String usuario,contraseña,correo;
+    public static String tvLongi,tvLati;
+    public int selecionado;
+
+    public usuarioActivo us=new usuarioActivo();
+    private DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
-
+        mAuth = FirebaseAuth.getInstance();
 
         regresar = (ImageView) findViewById(R.id.imageView);
         regresar.setOnClickListener(new View.OnClickListener() {
@@ -33,11 +67,186 @@ public class registrar extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
-                finish();
-                Intent intent = new Intent(getApplicationContext(), menu.class);
-                startActivity(intent);
+                registrarUsuario();
+
             }
         });
 
+        ingresarubicaciones=(Button)findViewById(R.id.button);
+        ingresarubicaciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ingresarubicaciones.setText("Ubicación agregada");
+                selecionado = 1;
+            }});
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Usuario =(EditText)findViewById(R.id.editText4);
+        Contraseña =(EditText)findViewById(R.id.correotext);
+        Correo =(EditText)findViewById(R.id.editText6);
+
     }
+
+    private void registrarUsuario(){
+
+        usuario= Usuario.getText().toString().trim();
+        contraseña= Contraseña.getText().toString().trim();
+        correo= Correo.getText().toString().trim();
+
+
+        us.setCorreo(correo);
+                // comprobar datos
+                if(TextUtils.isEmpty(correo)){
+                    Toast.makeText(this,"Se debe ingresar un correo",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(contraseña)){
+                    Toast.makeText(this,"Falta ingresar la contraseña",Toast.LENGTH_LONG).show();
+                    return;
+                }else if(contraseña.length()<8){
+                    Toast.makeText(this,"La contraseña debe tener por lo menos 8 caracteres",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(usuario)){
+                    Toast.makeText(this,"Falta ingresar el nombre de usuario",Toast.LENGTH_LONG).show();
+
+                    return;
+                }
+
+                if(selecionado!=1){
+                    Toast.makeText(this,"Falta agregar la ubicación",Toast.LENGTH_LONG).show();
+
+                    return;
+                }
+
+        //crear un usuario
+        //creating a new user
+        mAuth.createUserWithEmailAndPassword(correo, contraseña)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if (task.isSuccessful()) {
+                            finish();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            setRegistrarUsuario(usuario,correo,tvLati,tvLongi);
+                            Intent intent = new Intent(getApplicationContext(), menu.class);
+                            startActivity(intent);
+
+                        }else{
+                            Toast.makeText(registrar.this,"Datos incorrectos ",Toast.LENGTH_LONG).show();
+
+                        }
+
+                    }
+                });
+
+
+
+    }
+
+    //Para Almacenar os datos en un usuario activo
+    public void setRegistrarUsuario(String nombre, String correo, String latitud, String longitud){
+        String tipo="normal";
+        String id=databaseReference.push().getKey();
+        Usuario user=new Usuario(nombre,correo,longitud,latitud,tipo);
+        databaseReference.child("usuario").child(id).setValue(user);
+        Toast.makeText(registrar.this,"Se ha registrado el usuario ",Toast.LENGTH_LONG).show();
+
+        //Almacenar os datos en un usuario activo
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("usuario").addValueEventListener(new ValueEventListener(){
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+
+                    dataSnapshot.getChildren();
+
+                    for(DataSnapshot ds:dataSnapshot.getChildren()){
+
+                        //mejorar para obtener multiples resultados
+                        if(ds.child("correo").getValue().toString().equals(us.getCorreo())) {
+                            us.setUsuario(ds.child("usuario").getValue().toString());
+                            us.setLongitud(ds.child("longitud").getValue().toString());
+                            us.setLatitud(ds.child("latitud").getValue().toString());
+                            us.setTipouser(ds.child("tipouser").getValue().toString());
+
+
+                            return;
+                        }
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+    //obtener ubicacion
+
+    public void onResume() {
+        super.onResume();
+        getLocation();
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    public void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 5, (LocationListener) this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void CheckPermission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // Getting reference to TextView tv_longitude
+
+        tvLongi = String.valueOf(location.getLongitude());
+        tvLati = String.valueOf(location.getLatitude());
+
+    }
+
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(registrar.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider!" + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
 }
